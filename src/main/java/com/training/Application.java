13168fb20d17a568e1f3af1f3e8c2fd4e1b5da02
Gergo Czifra <gergoczifra91@gmail.com>
2021-08-31 12:26:06 +0200

    created review branch

--- src/main/java/com/training/Application.java
+++ src/main/java/com/training/Application.java
@@ -8,5 +8,6 @@ public class Application {
 
     public static void main(String[] args) {
         CircularBuffer<Integer> buffer = new CircularBuffer<>(8, Integer.class);
+        buffer.put(10);
     }
 }
